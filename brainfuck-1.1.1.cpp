96233b93fee8b407988733d590196555ca451808
WilhelmVonHindenburger <104843104+WilhelmVonHindenburger@users.noreply.github.com>
2023-02-12 21:01:22 -0500

    Cleaning up code

--- brainfuck-1.1.1.cpp
+++ brainfuck-1.1.1.cpp
@@ -1,16 +1,18 @@
 //THIS PROGRAM RELEASED INTO THE PUBLIC DOMAIN UNDER CREATIVE COMMONS ZERO 1.0 UNIVERSAL PUBLIC DOMAIN DEDICATION
-//Version 1.1
+//Version 1.1.1
 #include <iostream>
 using namespace std;
 int main(){
-    //bf: the string containing the Brainfuck program
+    
+    //bf is the code of the Brainfuck program.
     char bf[]=
-    "++++++++[>++++++++<-]>++++++++.---.+++++++..+++.+[----->++>+++++<<]>.>+++++++.--------.+++.------.--------."; //Prints HELLO WORLD
-    char instruction; // The currect instruction
-    int instrptr=0; //The instruction pointer
+    "++++++++[>++++++++<-]>++++++++.---.+++++++..+++.+[----->++>+++++<<]>.>+++++++.--------.+++.------.--------.";
+    char instruction;   // The current instruction
+    int instrptr=0;     //The instruction pointer
     int mem[30000]={0}; //Memory space to run it in
-    int memptr=0; //Pointer in memory
-    int looplayers=0; //How many layers of loops are in the loop being checked
+    int memptr=0;       //Pointer in memory
+    int looplayers=0;   //How many layers of loops are in the loop being checked
+    
     while(1){
         instruction=bf[instrptr]; //Read the current instruction
         looplayers=0;
@@ -19,12 +21,13 @@ int main(){
         cout << "Instruction pointer: " << instrptr << endl;
         cout << "Memory pointer: " << memptr << endl;
         cout << "Memory current value: " << mem[memptr] << endl;//*/
-        if(memptr < 0 || memptr >= 30000){ //SEGMENTATION FAULT
+        
+        //Error if accessing memory out of bounds (segmentation fault)
+        if(memptr < 0 || memptr >= 30000){
             cout << "Invalid memory was accessed.\n";
-            cout << "Instruction: " << instruction << endl;
-            cout << "Instruction pointer: " << instrptr << endl;
-            cout << "Memory pointer: " << memptr << endl;
-            cout << "Memory current value: " << mem[memptr] << endl;
+            cout << "Instruction: "         << instruction << endl;
+            cout << "Instruction pointer: " << instrptr    << endl;
+            cout << "Memory pointer: "      << memptr      << endl;
             return 0xC0000005;
         }
         switch(instruction){
@@ -44,11 +47,11 @@ int main(){
                 memptr--;
                 instrptr++;
                 break;
-            case '.': //Output
+            case '.': //Output a value
                 cout << static_cast<char>(mem[memptr]);
                 instrptr++;
                 break;
-            case ',': //Input
+            case ',': //Input a value
                 cin >> mem[memptr];
                 instrptr++;
                 break;
@@ -58,7 +61,7 @@ int main(){
                     while(1){
                         instrptr++;
                         if(bf[instrptr] == '[') looplayers++; //Nested loop check
-                        if(bf[instrptr] == ']') looplayers--;
+                        if(bf[instrptr] == ']') looplayers--; //
                         if(bf[instrptr] == ']' && looplayers == 0) break;
                     }
                 }
@@ -69,17 +72,16 @@ int main(){
                 while(1){ //Go back to start of matching loop
                     instrptr--;
                     if(bf[instrptr] == ']') looplayers++; //Nested loop check
-                    if(bf[instrptr] == '[') looplayers--;
+                    if(bf[instrptr] == '[') looplayers--; //
                     if(bf[instrptr] == '[' && looplayers == 0) break;
                 }
                 break;
             case '\0': //End of file
                 return 0;
                 break;
-            default: //Just move forward, for comments
+            default: //If the instruction isn't a Brainfuck instruction it's a comment and gets skipped over
             instrptr++;
         }
-        if(!instruction) return 0;
     }
     return 0;
 }
